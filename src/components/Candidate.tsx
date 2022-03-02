5bea37945aaba3427c1814de763c535a21a3c6d6
dasom <dasom228@gmail.com>
2022-03-03 00:32:33 +0900

    Add images

--- src/components/Candidate.tsx
+++ src/components/Candidate.tsx
@@ -6,19 +6,19 @@ interface CandidateProps {
 }
 
 export const Candidate: FC<CandidateProps> = ({candidate}) => {
-  const {name, description} = candidate
+  const {id, name, description, imageName} = candidate
   return (
     <>
-      <label htmlFor="lee" className="block mt-4 first:mt-0">
+      <label htmlFor={name + id} className="block mt-4 first:mt-0">
         <input
           type="radio"
-          id="lee"
+          id={name + id}
           name="candidate"
           value="lee"
           className="peer invisible absolute"
         />
-        <figure className="flex items-center rounded-xl p-4 bg-slate-100/10 border-slate-100/60 peer-checked:border-2 border-white-100 transition-all h-32 hover:cursor-pointer">
-          <img className="w-24 h-24 rounded-full" src="images/lee.png" alt="" />
+        <figure className="flex items-center rounded-xl p-4 bg-slate-100/10 border-slate-100/60 peer-checked:border-2 border-white-100 transition-all h-32 hover:cursor-pointer hover:bg-slate-100">
+          <img className="w-24 h-24 rounded-full" src={`images/${imageName}`} alt="" />
           <div className="pl-6 space-y-2">
             <blockquote>
               <p className="text-sm text-white leading-4">
