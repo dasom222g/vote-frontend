049b05a9f904b280b92c6cbf173db49b63b1190a
dasom <dasom228@gmail.com>
2022-03-02 14:57:43 +0900

    Set webpack version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
+
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
