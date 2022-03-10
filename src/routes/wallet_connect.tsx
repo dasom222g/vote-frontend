e252e43108937330b2bfde9528096442df5fd868
dasom <dasom228@gmail.com>
2022-03-11 01:28:09 +0900

    Add connect and change event with metamask

--- src/routes/wallet_connect.tsx
+++ src/routes/wallet_connect.tsx
@@ -12,7 +12,7 @@ interface WalletConnetProps {
 }
 
 const WalletConnet: FC<WalletConnetProps> = () => {
-  const [erroMessage, setErroMessage] = useState<string>('')
+  const [errorMessage, setErrorMessage] = useState<string>('')
   const [account, setAccount] = useState<string>('')
   const [balance, setBalance] = useState<string>('')
 
@@ -24,39 +24,53 @@ const WalletConnet: FC<WalletConnetProps> = () => {
     try {
       if (window.ethereum) {
         // metamask 프로그램 설치 되어 있는 경우
-        // 이미 metamask가 연결 되어 있는경우 request 가능함
+        // 이미 metamask가 연결 되어 있는경우 request 가능하며 연결될때까지 await함
         const [account]: string[] = await window.ethereum.request({
           method: 'eth_requestAccounts'
         })
-        setAccount(account)
-        const balance: string = await window.ethereum.request({
-          method: 'eth_getBalance',
-          params: [account, 'latest']
-        })
-        setBalance(ethers.utils.formatEther(balance))
+        handleAccountChange(account)
         return
       }
-      setErroMessage('Install MetaMask!')
+      setErrorMessage('Install MetaMask!')
     } catch(error) {
       console.error(error)
     }
   }
 
+  const handleAccountChange = async (account: string) => {
+    setAccount(account)
+    handleBalance(account.toString())
+  }
+
+  const handleBalance = async (account: string) => {
+    const balance: string = await window.ethereum.request({
+      method: 'eth_getBalance',
+      params: [account, 'latest']
+    })
+    setBalance(ethers.utils.formatEther(balance))
+  }
+
+  const handleChainChanged = (chainId: string) => {
+    // console.log('chainId', chainId)
+    window.location.reload()
+  }
+
+  window.ethereum.on('accountsChanged', handleAccountChange)
+  window.ethereum.on('chainChanged', handleChainChanged)
+
   // view
   return (
     <>
-      <div>
-        <button
-          type="button"
-          className={`bg-gradient-to-r from-indigo-500 via-pink-600 to-pink-500 text-slate-100 font-bold text-sm rounded-md w-full py-3 text-white`}
-          onClick={handleConnect}
-        >
-          Connet matamask
-        </button>
-      </div>
-      {erroMessage ? (
+      <button
+        type="button"
+        className={`bg-gradient-to-r from-indigo-500 via-pink-600 to-pink-500 text-slate-100 font-bold text-sm rounded-md w-full py-3 text-white`}
+        onClick={handleConnect}
+      >
+        Connet matamask
+      </button>
+      {errorMessage ? (
         <div className="mt-4">
-          <p className="text-white text-sm">{erroMessage}</p>
+          <p className="text-white text-sm">{errorMessage}</p>
         </div>
       ) : (
         <div>
