3762d7a25a45c122f21a3208574a2e6c801843f5
dasom <dasom228@gmail.com>
2022-03-11 18:45:03 +0900

    Add moralis and setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,18 @@
-import { Arbitrum, Config, DAppProvider, Kovan, Mainnet, Ropsten } from '@usedapp/core'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MoralisProvider } from 'react-moralis'
 import 'tailwindcss/tailwind.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-const config: Config = {
-  readOnlyChainId: Mainnet.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
-    [Ropsten.chainId]: 'https://ropsten.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
-    [Kovan.chainId]: 'https://kovan.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
-    [Arbitrum.chainId]: 'https://arb1.arbitrum.io/rpc',
-  },
-  multicallVersion: 2 as const,
-}
+const appId = 'P2qbmbZ0vnsYI9ipGvZhK510VJs8UVZyh7wSHJKp'
+const serverUrl = 'https://xowcffi7vsce.usemoralis.com:2053/server'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DAppProvider config={config}>
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
       <App />
-    </DAppProvider>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/routes/wallet-web3-react.tsx
+++ src/routes/wallet-web3-react.tsx
@@ -1,6 +1,5 @@
-import { useEtherBalance, useEthers } from '@usedapp/core'
-import { formatEther } from 'ethers/lib/utils'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { useMoralis } from 'react-moralis'
 
 declare global {
   interface Window {
@@ -8,26 +7,43 @@ declare global {
   }
 }
 
-interface WalletWeb3ReactProps {
-}
+interface WalletWeb3ReactProps {}
 
 const WalletWeb3React: FC<WalletWeb3ReactProps> = () => {
-  const { activateBrowserWallet, account } = useEthers()
-  const etherBalance = useEtherBalance(account)
+  const { authenticate, isAuthenticated } = useMoralis()
+  const [account, setAccount] = useState<string>('')
+
+  const login = async () => {
+    await authenticate({signingMessage: "Welcome my Voting app" })
+          .then(function (user) {
+            user && setAccount(user.get('ethAddress'))
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+  }
 
   // view
   return (
     <>
-        {/* onClick={() => activateBrowserWallet()} */}
-      <button
-        type="button"
-        className={`bg-gradient-to-r from-indigo-500 via-pink-600 to-pink-500 text-slate-100 font-bold text-sm rounded-md w-full py-3 text-white`}
-        onClick={() => activateBrowserWallet()}
-      >
-        Connect
-      </button>
-      {account && <p>Account: {account}</p>}
-      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+      {/* onClick={() => activateBrowserWallet()} */}
+      {!isAuthenticated ? (
+        <>
+          <div>
+            <button
+              type="button"
+              className={`bg-gradient-to-r from-indigo-500 via-pink-600 to-pink-500 text-slate-100 font-bold text-sm rounded-md w-full py-3 text-white`}
+              onClick={() => login()}
+            >
+              Connect
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>Account: {account}</h1>
+        </>
+      )}
     </>
   )
 }
